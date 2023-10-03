e486dc63f2004f1edc4e68545892c580ff5088b5
DIYA :) <146110147+DIYAAAAAAAAAAAAA@users.noreply.github.com>
2023-10-03 17:09:51 +0100

    testing

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+tiles.set_current_tilemap(tilemap("test1"))
+
 playerCharacter = sprites.create(img("""
     . . . . . . . . . . b 5 b . . .
     . . . . . . . . . b 5 b . . . .
@@ -17,10 +20,34 @@ playerCharacter = sprites.create(img("""
     . . . c c c c c c c c b b . . .
 """), SpriteKind.player)
 
-controller.move_sprite(playerCharacter)
+tiles.place_on_tile(playerCharacter, tiles.get_tile_location(10, 10))
 
-tiles.set_current_tilemap(tilemap("test1"))
+object1 = sprites.create(img("""
+    . . . . c c c b b b b b . . . .
+    . . c c b 4 4 4 4 4 4 b b b . .
+    . c c 4 4 4 4 4 5 4 4 4 4 b c .
+    . e 4 4 4 4 4 4 4 4 4 5 4 4 e .
+    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c
+    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e
+    e b b 4 4 4 4 4 4 4 4 4 4 4 b e
+    . e b 4 4 4 4 4 5 4 4 4 4 b e .
+    8 7 e e b 4 4 4 4 4 4 b e e 6 8
+    8 7 2 e e e e e e e e e e 2 7 8
+    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e
+    e c 6 7 6 6 7 7 7 6 6 7 6 c c e
+    e b e 8 8 c c 8 8 c c c 8 e b e
+    e e b e c c e e e e e c e b e e
+    . e e b b 4 4 4 4 4 4 4 4 e e .
+    . . . c c c c c e e e e e . . .
+"""), SpriteKind.food)
+
+controller.move_sprite(playerCharacter)
 
 scene.camera_follow_sprite(playerCharacter)
 
-tiles.set_wall_at(tiles.get_tile_location(0, 0), True)
+tiles.set_wall_at(tiles.get_tile_location(0, 0), True)     
+
+def on_overlap(sprite, otherSprite):
+    otherSprite.destroy()
+
+sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_overlap)
