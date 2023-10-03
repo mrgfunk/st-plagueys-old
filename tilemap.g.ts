d18097776d46ad9b70f8d81b78cb8613e040cd10
DIYA :) <146110147+DIYAAAAAAAAAAAAA@users.noreply.github.com>
2023-10-03 16:32:48 +0100

    Added test tilemap :)

---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "test1":
+            case "level1":return tiles.createTilemap(hex`1000100007080807010107070701010302020401080801070708070101070703020204070101010107070101070105020202060101070701070701070105020202060101010701010801010705020202060107070107010708070701030202040107070701010807070801050202020601010801070701070701010302020407070108010701080101070103020204070701070101010101010701030202040108070701010707070107050202020601010101010807010708010302020401080707070101010707010502020206010701070701010101080502020206010107070107010707010103020204070701080107070101070101030202040107010701080101`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tilePath5,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.castle.tilePath1,sprites.castle.tilePath9,sprites.castle.tileGrass1,sprites.castle.tileGrass3], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
