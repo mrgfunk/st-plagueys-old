aba8091b89af1ed465e7c05b5dae5154ce9ae6c6
mrgfunk <129779469+mrgfunk@users.noreply.github.com>
2023-10-31 15:36:37 +0000

    Update pxt.json, tilemap.g.jres, tilemap.g.ts, main.py, images.g.jres, images.g.ts

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
-playerCharacter = sprites.create(img("""
-    . . . . . . . . . . b 5 b . . .
-    . . . . . . . . . b 5 b . . . .
-    . . . . . . . . . b c . . . . .
-    . . . . . . b b b b b b . . . .
-    . . . . . b b 5 5 5 5 5 b . . .
-    . . . . b b 5 d 1 f 5 5 d f . .
-    . . . . b 5 5 1 f f 5 d 4 c . .
-    . . . . b 5 5 d f b d d 4 4 . .
-    b d d d b b d 5 5 5 4 4 4 4 4 b
-    b b d 5 5 5 b 5 5 4 4 4 4 4 b .
-    b d c 5 5 5 5 d 5 5 5 5 5 b . .
-    c d d c d 5 5 b 5 5 5 5 5 5 b .
-    c b d d c c b 5 5 5 5 5 5 5 b .
-    . c d d d d d d 5 5 5 5 5 d b .
-    . . c b d d d d d 5 5 5 b b . .
-    . . . c c c c c c c c b b . . .
+tiles.set_current_tilemap(tilemap("""test1"""))
+
+player_character = sprites.create(img("""
+    . . . . . f f 4 4 f f . . . . .
+    . . . . f 5 4 5 5 4 5 f . . . .
+    . . . f e 4 5 5 5 5 4 e f . . .
+    . . f b 3 e 4 4 4 4 e 3 b f . .
+    . . f 3 3 3 3 3 3 3 3 3 3 f . .
+    . f 3 3 e b 3 e e 3 b e 3 3 f .
+    . f 3 3 f f e e e e f f 3 3 f .
+    . f b b f b f e e f b f b b f .
+    . f b b e 1 f 4 4 f 1 e b b f .
+    f f b b f 4 4 4 4 4 4 f b b f f
+    f b b f f f e e e e f f f b b f
+    . f e e f b d d d d b f e e f .
+    . . e 4 c d d d d d d c 4 e . .
+    . . e f b d b d b d b b f e . .
+    . . . f f 1 d 1 d 1 d f f . . .
+    . . . . . f f b b f f . . . . .
 """), SpriteKind.player)
 
-controller.move_sprite(playerCharacter)
+burger = sprites.create(img("""
+    . . . . c c c b b b b b . . . .
+    . . c c b 4 4 4 4 4 4 b b b . .
+    . c c 4 4 4 4 4 5 4 4 4 4 b c .
+    . e 4 4 4 4 4 4 4 4 4 5 4 4 e .
+    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c
+    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e
+    e b b 4 4 4 4 4 4 4 4 4 4 4 b e
+    . e b 4 4 4 4 4 5 4 4 4 4 b e .
+    8 7 e e b 4 4 4 4 4 4 b e e 6 8
+    8 7 2 e e e e e e e e e e 2 7 8
+    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e
+    e c 6 7 6 6 7 7 7 6 6 7 6 c c e
+    e b e 8 8 c c 8 8 c c c 8 e b e
+    e e b e c c e e e e e c e b e e
+    . e e b b 4 4 4 4 4 4 4 4 e e .
+    . . . c c c c c e e e e e . . .
+"""), SpriteKind.food)
 
-tiles.set_current_tilemap(tilemap("test1"))
+tiles.place_on_tile(burger, tiles.get_tile_location(0, 10))
+scene.camera_follow_sprite(player_character)
 
-scene.camera_follow_sprite(playerCharacter)
+def on_overlap_player_food(sprite, otherSprite):
+    sprite.start_effect(effects.spray)
+    otherSprite.start_effect(effects.fire)
 
-tiles.set_wall_at(tiles.get_tile_location(0, 0), True)
+sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_overlap_player_food)
+
+
+controller.move_sprite(player_character)
